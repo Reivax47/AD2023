95a0b2d7e0ddf6979b4c8dd96578a4ccbd0d44ac
Xavier Monset <xavier.monset@gmail.com>
2023-12-04 09:28:50 +0100

    Prep Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,32 @@
+import kotlin.math.pow
+
+fun main() {
+
+
+    fun part1(input: List<String>): Int {
+
+        var reponse = 0
+
+        return reponse
+    }
+
+
+    fun part2(input: List<String>): Long {
+
+        var reponse = 0L
+
+        return reponse
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day05_test")
+    check(part1(testInput) == 13)
+
+//    testInput = readInput("Day01_test2")
+    check(part2(testInput) == 30L)
+
+    val input = readInput("Day05")
+    part1(input).println()
+    part2(input).println()
+}
+
